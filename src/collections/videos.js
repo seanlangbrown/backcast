1258d13df2e807d1dc636bcf387d886b8402973f
seanlangbrown <seanlangbrown@gmail.com>
2018-01-30 20:50:13 -0800

    complete APIhelper in videos collection

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,29 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+  url: 'https://www.googleapis.com/youtube/v3/search/?q=surfing&maxResults=5&type=video&part=snippet&key=' + window.APIKey + '&videoEmbeddable=true',
+
+  search: function(query) {
+    //console.log(window.APIKey);
+    this.fetch({
+      type: 'GET',
+      data: {
+        part: 'snippet',
+        key: window.APIKey,
+        q: query,
+        maxResults: 5,
+        type: 'video',
+        videoEmbeddable: 'true'
+      },
+      success: function(data) {
+        console.log(data);
+      },
+    });
+  },
+
+  parse: function(response) {
+    console.log('parsing');
+    return response.items;
+  }
 
 });
--- src/models/video.js
+++ src/models/video.js
@@ -2,7 +2,8 @@ var Video = Backbone.Model.extend({
 
   initialize: function(video) {
     // override youtube's complex id field
-    this.set('id', video.id.videoId);
+    //console.log(video);
+    this.set('id', video.id.videoId || video.id);
   },
 
   select: function() {
